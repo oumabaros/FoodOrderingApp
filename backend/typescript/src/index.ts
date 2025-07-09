7cecf92fc71a4394f01df3cff5e076f9e901e80a
Barasa Ouma <oumabaros@gmail.com>
2025-07-09 04:59:53 +0300

    First Commit

--- backend/typescript/src/index.ts
+++ backend/typescript/src/index.ts
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.get('/test', async (req: Request, res: Response) => {
-  res.json({message: 'Hello World There!'});
+  res.json({message: 'Hello World!'});
 });
 
 app.listen(7000, () => {

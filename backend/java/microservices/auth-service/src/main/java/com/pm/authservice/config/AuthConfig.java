687bdf544c8f738f29d5ad352464aec50992779e
Barasa Ouma <oumabaros@gmail.com>
2025-07-12 15:18:01 +0300

    Auth0

--- backend/java/microservices/auth-service/src/main/java/com/pm/authservice/config/AuthConfig.java
+++ backend/java/microservices/auth-service/src/main/java/com/pm/authservice/config/AuthConfig.java
@@ -1,19 +1,43 @@
 package com.pm.authservice.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import static org.springframework.security.config.Customizer.withDefaults;
+import java.io.IOException;
 
 @Configuration
 public class AuthConfig {
+    @Value("${auth0.issuer}")
+    private String issuer;
+    @Value("${oauth0.client-id}")
+    private String clientId;
+
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http.authorizeHttpRequests(auth -> auth
-                        .anyRequest()
-                        .authenticated())
-                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults())).build();
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/", "/images/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2Login(withDefaults())
+                .logout(logout -> logout
+                        .addLogoutHandler(logoutHandler()));
+        return http.build();
     }
 
+    private LogoutHandler logoutHandler() {
+        return (request, response, authentication) -> {
+            try {
+                String baseUrl = ServletUriComponentsBuilder.fromCurrentContextPath().build().toUriString();
+                response.sendRedirect(issuer + "v2/logout?client_id=" + clientId + "&returnTo=" + baseUrl);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        };
+    }
 }

f4d87dee2c9134fa438f7294878f48b585ec465a
Barasa Ouma <oumabaros@gmail.com>
2025-07-20 00:22:12 +0300

    CORS

--- backend/java/microservices/auth-service/src/main/java/com/pm/authservice/controller/HomeController.java
+++ backend/java/microservices/auth-service/src/main/java/com/pm/authservice/controller/HomeController.java
@@ -2,7 +2,13 @@ package com.pm.authservice.controller;
 
 import com.pm.authservice.config.AuthProperties;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+
 
 @RestController
 public class HomeController {
@@ -15,5 +21,12 @@ public class HomeController {
     @GetMapping("/")
     public String getGreeting() {
         return authProperties.getGreeting();
+    }
+
+    @RequestMapping(value = "/ping", method = RequestMethod.GET)
+    public String ping() {
+        SecurityContext context = SecurityContextHolder.getContext();
+        Authentication authentication = context.getAuthentication();
+        return "Scopes: " + authentication.getAuthorities();
     }
 }

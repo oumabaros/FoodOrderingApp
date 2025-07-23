43e0b82fee129c0203e4de6f20f81e3bba0c2836
Barasa Ouma <oumabaros@gmail.com>
2025-07-23 08:19:18 +0300

    Added Update User

--- backend/typescript/src/index.ts
+++ backend/typescript/src/index.ts
@@ -16,6 +16,10 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.get('/health', async (req: Request, res: Response) => {
+  res.send({message: 'Health OK!'});
+});
+
 app.use('/api/my/user', myUserRoute);
 app.listen(7000, () => {
   console.log('Server started on localhost:7000');
--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -1,5 +1,6 @@
 export type User = {
   _id: string;
+  id: string;
   email: string;
   name: string;
   addressLine1: string;

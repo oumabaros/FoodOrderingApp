ffb7eb49154e53a565db57f521389bef3dc04270
Barasa Ouma <oumabaros@gmail.com>
2025-08-07 02:48:00 +0300

    Added Restaurant Search

--- backend/java/microservices/restaurant-service/src/main/java/com/pm/restaurantservice/config/SecurityConfig.java
+++ backend/java/microservices/restaurant-service/src/main/java/com/pm/restaurantservice/config/SecurityConfig.java
@@ -13,7 +13,9 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
+        http.authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/public/**", "/actuator/**","/restaurant/search/**").permitAll()
+                        .anyRequest().authenticated())
                 .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()));
         return http.build();
     }

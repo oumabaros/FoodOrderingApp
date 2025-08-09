188a8ca802a48215e32d8cbf7f0199563c8ed1c9
Barasa Ouma <oumabaros@gmail.com>
2025-08-09 14:01:20 +0300

    Added Restaurant Search

--- frontend/src/pages/DetailPage.tsx
+++ frontend/src/pages/DetailPage.tsx
@@ -1,8 +1,16 @@
 import { useGetRestaurant } from "@/api/RestaurantApi";
+
+import OrderSummary from "@/components/OrderSummary";
 import RestaurantInfo from "@/components/RestaurantInfo";
-import { AspectRatio } from "@radix-ui/react-aspect-ratio";
-//import { useState } from "react";
+import { AspectRatio } from "@/components/ui/aspect-ratio";
+import { Card, CardFooter } from "@/components/ui/card";
+import { useState } from "react";
 import { useParams } from "react-router-dom";
+import { type MenuItem as MenuItemType } from "../types";
+import CheckoutButton from "@/components/CheckoutButton";
+import { useCreateCheckoutSession } from "@/api/OrderApi";
+import type { UserFormData } from "@/forms/user-profile-form/UserProfileForm";
+import MenuItem from "@/components/MenuItem";
 
 export type CartItem = {
   _id: string;
@@ -14,6 +22,88 @@ export type CartItem = {
 const DetailPage = () => {
   const { restaurantId } = useParams();
   const { restaurant, isLoading } = useGetRestaurant(restaurantId);
+  const { createCheckoutSession, isLoading: isCheckoutLoading } =
+    useCreateCheckoutSession();
+
+  const [cartItems, setCartItems] = useState<CartItem[]>(() => {
+    const storedCartItems = sessionStorage.getItem(`cartItems-${restaurantId}`);
+    return storedCartItems ? JSON.parse(storedCartItems) : [];
+  });
+
+  const addToCart = (menuItem: MenuItemType) => {
+    setCartItems((prevCartItems) => {
+      const existingCartItem = prevCartItems.find(
+        (cartItem) => cartItem._id === menuItem._id
+      );
+
+      let updatedCartItems;
+
+      if (existingCartItem) {
+        updatedCartItems = prevCartItems.map((cartItem) =>
+          cartItem._id === menuItem._id
+            ? { ...cartItem, quantity: cartItem.quantity + 1 }
+            : cartItem
+        );
+      } else {
+        updatedCartItems = [
+          ...prevCartItems,
+          {
+            _id: menuItem._id,
+            name: menuItem.name,
+            price: menuItem.price,
+            quantity: 1,
+          },
+        ];
+      }
+
+      sessionStorage.setItem(
+        `cartItems-${restaurantId}`,
+        JSON.stringify(updatedCartItems)
+      );
+
+      return updatedCartItems;
+    });
+  };
+
+  const removeFromCart = (cartItem: CartItem) => {
+    setCartItems((prevCartItems) => {
+      const updatedCartItems = prevCartItems.filter(
+        (item) => cartItem._id !== item._id
+      );
+
+      sessionStorage.setItem(
+        `cartItems-${restaurantId}`,
+        JSON.stringify(updatedCartItems)
+      );
+
+      return updatedCartItems;
+    });
+  };
+
+  const onCheckout = async (userFormData: UserFormData) => {
+    if (!restaurant) {
+      return;
+    }
+
+    const checkoutData = {
+      cartItems: cartItems.map((cartItem) => ({
+        menuItemId: cartItem._id,
+        name: cartItem.name,
+        quantity: cartItem.quantity.toString(),
+      })),
+      restaurantId: restaurant._id,
+      deliveryDetails: {
+        name: userFormData.name,
+        addressLine1: userFormData.addressLine1,
+        city: userFormData.city,
+        country: userFormData.country,
+        email: userFormData.email as string,
+      },
+    };
+
+    const data = await createCheckoutSession(checkoutData);
+    window.location.href = data.url;
+  };
 
   if (isLoading || !restaurant) {
     return "Loading...";
@@ -31,16 +121,17 @@ const DetailPage = () => {
         <div className="flex flex-col gap-4">
           <RestaurantInfo restaurant={restaurant} />
           <span className="text-2xl font-bold tracking-tight">Menu</span>
-          {/* {restaurant.menuItems.map((menuItem) => (
+          {restaurant.menuItems.map((menuItem, index) => (
             <MenuItem
               menuItem={menuItem}
               addToCart={() => addToCart(menuItem)}
+              key={index}
             />
-          ))} */}
+          ))}
         </div>
 
         <div>
-          {/* <Card>
+          <Card>
             <OrderSummary
               restaurant={restaurant}
               cartItems={cartItems}
@@ -53,7 +144,7 @@ const DetailPage = () => {
                 isLoading={isCheckoutLoading}
               />
             </CardFooter>
-          </Card> */}
+          </Card>
         </div>
       </div>
     </div>
